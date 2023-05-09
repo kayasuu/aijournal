5f3573d8ab64215f141e88dbdb8299178b935be9
sude <sude@sudes-MacBook-Pro.local>
2023-05-09 20:56:45 +1000

    change pg

--- models/common.py
+++ models/common.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 def sql_read(query, parameters):
-    connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("PGUSER"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
+    connection = psycopg2.connect(host=os.getenv("PGHOST"), user="pg", password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
     cursor = connection.cursor()
     cursor.execute(query, parameters)
     results = cursor.fetchall()
@@ -9,7 +9,7 @@ def sql_read(query, parameters):
     return results
 
 def sql_write(query, parameters):
-    connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("PGUSER"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
+    connection = psycopg2.connect(host=os.getenv("PGHOST"), user="pg", password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
     cursor = connection.cursor()
     cursor.execute(query, parameters)
     connection.commit()
