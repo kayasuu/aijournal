399a75bcf0ac2349d1a0748bcf666c49b9e0ea92
sude <sude@sudes-MacBook-Pro.local>
2023-05-13 00:18:02 +1000

    changes

--- users.sql
+++ users.sql
@@ -3,4 +3,3 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(id SERIAL PRIMARY KEY, email TEXT NOT NULL, user_name TEXT NOT NULL, password_hash TEXT NOT NULL);
 
 INSERT INTO users(email, user_name) VALUES ('admin@gmail.com', 'admin', 'test');
--- admin@gmail.com, password, admin1@gmail.com, pass
